b906232c9500c566fd3d23ed884a473d3b2441ab
unknown <gremcleo@gmail.com>
2020-01-04 17:27:41 -0500

    init

---
+++ CSharp-Course-Intermediate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Course_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
