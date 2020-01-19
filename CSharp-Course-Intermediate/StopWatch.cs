491e14c1e9b15ce49d21b2d2571c3517db195b1e
unknown <gremcleo@gmail.com>
2020-01-19 12:54:52 -0500

    stopwatch exercise representing object state

---
+++ CSharp-Course-Intermediate/StopWatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CSharp_Course_Intermediate
+{
+    public class StopWatch
+    {
+        public TimeSpan TimeSpan { get; private set; }
+        private DateTime StarTTime { get; set; }
+
+        private DateTime StopTime { get; set; }
+
+        public void Start()
+        {
+            try
+            {
+                this.StarTTime = DateTime.Now;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                this.StopTime = DateTime.Now;
+                TimeSpan = StopTime - StarTTime;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public StopWatch()
+        {
+            this.TimeSpan = new TimeSpan();
+            this.StarTTime = new DateTime();
+            this.StopTime = new DateTime();
+        }
+
+    }
+}
