0edd18b472111ca554fa82d63bd28fe3b7e619b3
unknown <gremcleo@gmail.com>
2020-01-19 12:55:44 -0500

    post exercise representing properties and encapsulation

---
+++ CSharp-Course-Intermediate/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharp_Course_Intermediate
+{
+    public class Post
+    {
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        private int Votes { get; set; }
+
+        public Post()
+        {
+        }
+
+        public int UpVote()
+        {
+            return ++Votes;
+        }
+
+        public int DownVote()
+        {
+            return --Votes;
+        }
+    }
+}
