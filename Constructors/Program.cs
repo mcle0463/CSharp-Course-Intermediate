3ef8f63ca9e32f7c1d37654195656223bf65a7ae
unknown <gremcleo@gmail.com>
2020-01-26 11:40:01 -0500

    remove class declaration

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -1,28 +1,5 @@
-﻿using System;
-
-namespace Constructors
+﻿namespace Constructors
 {
-    public class Vehicle
-    {
-        private string RegistrationNumber { get; set; }
-
-        public Vehicle(string registrationNumber)
-        {
-
-            this.RegistrationNumber = registrationNumber;
-            Console.WriteLine("vehicle is being initialized{0} ", registrationNumber);
-        }
-    }
-
-    public class Car : Vehicle
-    {
-        public string RegistrationNumber { get; set; }
-        public Car(string registrationNumber): base(registrationNumber)
-        {
-            this.RegistrationNumber = registrationNumber;
-            Console.WriteLine("car is being initialized{0}",registrationNumber);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
