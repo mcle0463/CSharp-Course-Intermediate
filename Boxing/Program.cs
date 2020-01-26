04e8f8693b8ca5b476ab541920433d5ba9aba5e8
unknown <gremcleo@gmail.com>
2020-01-26 11:39:11 -0500

    add new course files

---
+++ Boxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("mosh");
+            list.Add(new DateTime());
+
+            var number = (int) list[1];
+            var list2 = new List<int>();
+
+        }
+    }
+}
---
+++ Casting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StreamReader reader = new StreamReader(new MemoryStream());
+            Shape shape = new Text();
+            Text text = (Text) shape;
+            
+
+        }
+    }
+}
