3de44c5b46fd9950de6aed0d38700ae1aa1c3c1b
unknown <gremcleo@gmail.com>
2020-01-26 08:34:13 -0500

    Car to new class file

---
+++ Constructors/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Car : Vehicle
+    {
+        public string RegistrationNumber { get; set; }
+        public Car(string registrationNumber): base(registrationNumber)
+        {
+            this.RegistrationNumber = registrationNumber;
+            Console.WriteLine("car is being initialized{0}",registrationNumber);
+        }
+    }
+}
