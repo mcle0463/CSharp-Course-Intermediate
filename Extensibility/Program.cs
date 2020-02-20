7c9761861ebb95f586f56ae7c74306efd43b7b0b
gremcleo <gremcleo@gmail.com>
2020-02-19 20:19:42 -0500

    Interface extensibility with DbConnector

---
+++ Extensibility/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\log.txt"));
+            dbMigrator.Migrate();
+            //var DbMigrator1 = new DbMigrator(new FileLogger());
+        }
+    }
+}
